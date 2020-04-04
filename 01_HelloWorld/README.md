7c4e25ae153f91f0a3d904dd3f1237afabb70c63
Clement Chaudat <clement-c@mpcfilm.com>
2020-04-04 11:45:39 +0100

    With GLEW working

--- 01_HelloWorld/README.md
+++ 01_HelloWorld/README.md
@@ -1,1 +1,19 @@
-# OpenGL experiments
+﻿# OpenGL experiments
+
+
+## Set up
+
+Expects `glfw3` and `glew` installed in `C:/Dev/Libs`
+
+```
+C:/Dev/Libs
+├──include
+│   └──GLFW
+│   └──GL
+├──lib32
+├──lib32-vc2017
+├──lib32-vc2019
+├──lib64
+├──lib64-vc2017
+└──lib64-vc2019
+```
--- 01_HelloWorld/src/Main.cpp
+++ 01_HelloWorld/src/Main.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include <iostream>
 
 
@@ -6,21 +8,34 @@ int main(int argc, char** argv)
 {
 	GLFWwindow* win;
 
-	if (!glfwInit()) // I always forget....
+	// GLFW init, I always forget it
+	if (!glfwInit())
 	{
-		std::cerr << "Error while initaiting GLFW" << std::endl;
+		std::cerr << "Error while initializing GLFW" << std::endl;
 		return -1;
 	}
 
+	// Create the context before initializing GLEW
 	win = glfwCreateWindow(480, 320, "Hello World - " PLATFORM_SHORT, nullptr, nullptr);
+	glfwMakeContextCurrent(win);
 	if (!win)
 	{
 		std::cerr << "Error while creating the window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-	glfwMakeContextCurrent(win);
 
+	// Glew init
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Error while initializing GLEW" << std::endl;
+		return -1;
+	}
+
+	// Pring version wrangled by GLEW
+	std::cout << "Initialized using " << glGetString(GL_VERSION) << std::endl;
+
+	// Main loop
 	while (!glfwWindowShouldClose(win))
 	{
 		if (glfwGetKey(win, GLFW_KEY_ESCAPE)) // Bind escape key to close the window
@@ -39,6 +54,8 @@ int main(int argc, char** argv)
 		glfwSwapBuffers(win);
 		glfwPollEvents();
 	}
+
+	// Destroy everything
 	glfwTerminate();
 
 	return 0;
